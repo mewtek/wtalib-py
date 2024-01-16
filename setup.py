ba4943491454bd313dedf23b63068eee8b2b41cb
April Wormwood <mewtek@icloud.com>
2024-01-15 21:38:47 -0800

    Update package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="wtalib-py",
+    name="wtalib",
     packages=find_packages(include=['wtalib-py']),
     version="0.0.1",
     description="Asynchronous python library for the Whatcom Transit Authority API",
