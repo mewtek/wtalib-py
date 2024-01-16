657c9972142bc837bb314824ba58513b57102926
April Wormwood <mewtek@icloud.com>
2024-01-15 21:56:14 -0800

    Add test requirements into setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import find_packages, setup
 
 setup(
     name="wtalib",
-    packages=find_packages(include=['wtalib-py']),
+    packages=find_packages(include=['wtalib']),
     version="0.0.1",
     description="Asynchronous python library for the Whatcom Transit Authority API",
     author="mewtek",
     install_requires=['aiohttp'],
+    setup_requires=['pytest-runner'],
     tests_require=['pytest', 'pytest-asyncio'],
     test_suite='tests'
 )
