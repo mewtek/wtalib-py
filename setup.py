279b68bf1f6faf14e8642a3b0a7504227ac2a710
April Wormwood <mewtek@icloud.com>
2024-01-15 21:33:29 -0800

    Update setup.py with package information

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="wtalib-py",
+    packages=find_packages(include=['wtalib-py']),
+    version="0.0.1",
+    description="Asynchronous python library for the Whatcom Transit Authority API",
+    author="mewtek",
+    install_requires=['aiohttp'],
+    tests_require=['pytest', 'pytest-asyncio'],
+    test_suite='tests'
+)
